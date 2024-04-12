cdc623672a03aae689268f21d8c88e5aba103a4a
TimB2003 <54979904+TimB2003@users.noreply.github.com>
2024-04-12 09:28:24 +0200

    gamepad now works, keyboard still has some problems

--- src/input_system/keyboard.rs
+++ src/input_system/keyboard.rs
@@ -1,12 +1,14 @@
+use bevy::prelude::*;
+
 use super::input_handler;
-use bevy::prelude::{ButtonInput, DetectChanges, KeyCode, Res};
+
 mod button_definitions {
     use bevy::prelude::KeyCode;
 
     //defines the different buttons used as well as their usage
     pub const JUMP_BUTTON: &KeyCode = &KeyCode::Space;
 }
-pub fn keyboard_input(keyboard_input: ButtonInput<KeyCode>) {
+pub fn keyboard_input(keyboard_input: Input<KeyCode>) {
     for ev in keyboard_input.get_pressed() {
         match ev {
             button_definitions::JUMP_BUTTON => {
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ mod input_system;
 
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(
             Update,
             (
                 input_system::gamepad::gamepad_connections,
                 input_system::gamepad::gamepad_input,
-                input_system::keyboard::keyboard_input,
+                //input_system::keyboard::keyboard_input,
             ),
         )
-        .run()
+        .run();
 }
