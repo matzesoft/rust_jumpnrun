d44391aa25c8a23d54b4388444d2689d773ac5c2
matzesoft <matzesoft@gmail.com>
2024-04-19 09:40:13 +0200

    Update start script to only run wasm if cargo was sucessful

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cargo run --target wasm32-unknown-unknown --all-features
-wasm-server-runner ./target/wasm32-unknown-unknown/debug/rust_jumpnrun.wasm
+
+# Check if cargo run was successful
+if [ $? -eq 0 ]; then
+    wasm-server-runner ./target/wasm32-unknown-unknown/debug/rust_jumpnrun.wasm
+fi
