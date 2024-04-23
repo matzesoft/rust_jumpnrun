b7032deb559ef6c9130dbe1b0c732cde71af806d
matzesoft <matzesoft@gmail.com>
2024-04-23 14:39:46 +0200

    Remove WASM

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cargo run --target wasm32-unknown-unknown --all-features
-
-# Check if cargo run was successful
-if [ $? -eq 0 ]; then
-    wasm-server-runner ./target/wasm32-unknown-unknown/debug/rust_jumpnrun.wasm
-fi
